ab831bd1a4421c6ec9553c33c9e61056d8b8e5e0
bruh315 <tsaavaalex555@gmail.com>
2026-02-13 20:51:01 +0400

    new features & fixed linking error

--- crt.h
+++ crt.h
@@ -3,9 +3,14 @@
 
 #define C__MEMSIZE_ 10 * 1024 * 1024
 
-extern char C__MEMORY_[C__MEMSIZE_];
-extern unsigned int C__MEMPTR_ = C__MEMSIZE_;
-extern unsigned int C__SEED_ = 0;
+#ifndef CRT__VARS
+#define CRT__VARS
+    char C__MEMORY_[C__MEMSIZE_];
+    unsigned int C__MEMPTR_ = C__MEMSIZE_;
+    int C__SEED_ = 0; 
+#endif
+
+#define c_silent_assert(cond) while (!(cond));
 
 #ifndef true
     #define true 1
@@ -19,10 +24,10 @@ extern unsigned int C__SEED_ = 0;
     #define bool int
 #endif
 
-static char *c_malloc(const unsigned int size) {
+extern char *c_malloc(const unsigned int size) {
     if (C__MEMPTR_ <= size || size < 1) return 0;
     C__MEMPTR_ -= size;
-    return (char *) (C__MEMPTR + C__MEMORY_);
+    return (char *) (C__MEMPTR_ + C__MEMORY_);
 }
 
 static int c_memset(char *const buf, const unsigned int size, const char value) {
@@ -32,7 +37,7 @@ static int c_memset(char *const buf, const unsigned int size, const char value) 
     return 1;
 }
 
-static char *c_calloc(const unsigned int size) {
+extern char *c_calloc(const unsigned int size) {
     char *p = c_malloc(size);
     if (!p) return p;
     unsigned int index;
@@ -40,14 +45,28 @@ static char *c_calloc(const unsigned int size) {
     return p;
 }
 
-static char *c_realloc(char *const old_ptr, const unsigned int old_size, const unsigned int new_size) {
-    if (!old_ptr || !old_size || !new_sizes) return c_calloc(new_size);
+extern char *c_realloc(char *const old_ptr, const unsigned int old_size, const unsigned int new_size) {
+    if (!old_ptr || !old_size) return c_calloc(new_size);
     if (old_size == new_size) return old_ptr;
     char *new_ptr = c_calloc(new_size);
     if (!new_ptr) return new_ptr;
     unsigned int index;
     for (index = new_size > old_size ? old_size : new_size; index --> 0;) new_ptr[index] = old_ptr[index];
     return new_ptr;
+}
+
+extern void c_free(const char *const p) {
+    // TODO: implement c_free(const char *const p) {
+        //TODO: implement c_free(const char *const p) {
+            //TODO: implement c_free(const char *const p) {
+                //TODO: implement c_free(const char *const p) {
+                    //TODO: implement c_free(const char *const p) {
+                        //...
+                    //}
+                //}
+            //}
+        //}
+    //}
 }
 
 static int c_memcpy(char *const dest_buf, const char *const src_buf, const unsigned int dest_size, const unsigned int src_size) {
@@ -93,17 +112,20 @@ static int c_strchr(const char *const haystack, const char needle) {
     return -1;
 }
 
-typedef struct {
-    char *stack;
+struct c_coroutine;
+
+typedef struct c_coroutine {
+    int *stack;
     unsigned int stack_size;
     unsigned int esp;
     int state;
-    void (*handler)(c_coroutine *);
+    void (*handler)(struct c_coroutine *);
 } c_coroutine;
 
 static c_coroutine c_coroutine_make(const unsigned int stack_size, const int initial_state, void (*handler)(c_coroutine *)) {
     c_coroutine routine = {0};
-    routine.stack = c_calloc(stack_size);
+    routine.stack = (int *) c_calloc(stack_size * sizeof(int));
+    c_silent_assert(routine.stack);
     routine.stack_size = stack_size;
     routine.state = initial_state;
     routine.handler = handler;
@@ -123,7 +145,7 @@ static int c_rand()
     return C__SEED_;
 }
 
-#define c_coroutine_yield(routine, new_state)\ 
+#define c_coroutine_yield(routine, new_state) \
     do {\
         (routine).state = (new_state);\
         return;\
@@ -134,7 +156,82 @@ static int c_rand()
 
 static void c_coroutine_schedule(c_coroutine *routines, unsigned int count) {
     unsigned int index = 0;
-    for (index = 0; index < count; ++index) c_coroutine_tick(&routines[index]);
+    for (index = 0; index < count; ++index) (routines[index]).handler(&(routines[index]));;
 }
+
+typedef struct {
+    int *items;
+    unsigned int count; // Items pushed
+    unsigned int capacity; // Memory allocated
+} c_da;
+
+static c_da c_da_make() {
+    c_da da = {0};
+    return da;
+}
+
+static int c_da_append(c_da *const da, const int element) {
+    if (!da) return 0;
+    if (da->count >= da->capacity) {
+        if (da->capacity == 0) da->capacity = 20;
+        else da->capacity *= 2;
+        int *const p = (int *)c_realloc((char *)da->items, da->count * sizeof(int), da->capacity * sizeof(int));
+        if (!p) return 0;
+        da->items = p;
+    }
+    da->items[da->count++] = element;
+    return 1;
+}
+
+static int c_da_pop(c_da *const da, int *const out) {
+    if (!da || da->count < 1) return 0;
+    *out = da->items[--da->count];
+    return 1;
+}
+
+static int c_da_find(c_da *const haystack, const int element) {
+    if (!haystack) return -2;
+    unsigned int index = 0;
+    for (index = 0; index < haystack->count; ++index) if (haystack->items[index] == element) return index;
+    return -1;
+}
+
+static int c_da_reserve(c_da *const da, const unsigned int new_size) {
+    if (!da || !new_size) return 0;
+    int *const p = (int *const)c_realloc((char *)da->items, da->count * sizeof(int), new_size * sizeof(int));
+    if (!p) return 0;
+    da->items = p;
+    da->capacity = new_size;
+    return 1;
+}
+
+typedef struct {
+    void *ref;
+    int is_valid;
+} c_handle;
+
+static c_handle c_handle_load(void *const p) {
+    c_handle ref;
+    ref.is_valid = 1;
+    ref.ref = p;
+    return ref;
+}
+
+#define c_handle_set(handle, type, item)\
+    do {\
+        if (((handle).ref) && (handle).is_valid) (*((type *)(handle).ref)) = item;\
+    } while (0)
+
+#define c_handle_get(handle, type, out)\
+    do {\
+        if ((handle).ref && (handle).is_valid) out = *(((type) *)(handle).ref);\
+        else (out) = (type) 0;\
+    } while (0)
+
+#define c_handle_release(handle)\
+    do {\
+        (handle).ref = (void *)0;\
+        (handle).is_valid = 0;\
+    } while (0)
 
 #endif
